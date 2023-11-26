e8dfa40ce0ca3d86edc17f257f3452488b8e6479
Luis Hidalgo Aguilar <119078933+luishidalgoa@users.noreply.github.com>
2023-11-26 13:59:00 +0100

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,16 +1,16 @@
 export const environment = {
-  apis: {
-    audius: {
-      clientId: '7fbde568a2c420c60696481ed174fc3f8b816893',
-      clientSecret: '022b4597b97971b6f2f17398227802b4fef7d7705674ec3f29900c1d536c4caa'
+    apis: {
+      audius: {
+        clientId: '7fbde568a2c420c60696481ed174fc3f8b816893',
+        clientSecret: '022b4597b97971b6f2f17398227802b4fef7d7705674ec3f29900c1d536c4caa'
+      },
+      spotify: {
+        clientId: '0a1e3b2cba2a474098e259b2526fa484',
+        clientSecret: 'eb64f20c2c1c495682d9ba721b6d076a',
+      },
+      countryStateCity: {
+        key: 'Smh5aTVid29mc0FJcDZiWU5zeGVSeHhSdXNIZFUwMDFRZ2pweTZpUQ==',
+      },
     },
-    spotify: {
-      clientId: '72ea433aa55b4c71a3b7a8c082dbab57',
-      clientSecret: 'fcb3200f35de457dbd966940b5f86893',
-    },
-    countryStateCity: {
-      key: 'Smh5aTVid29mc0FJcDZiWU5zeGVSeHhSdXNIZFUwMDFRZ2pweTZpUQ==',
-    },
-  },
-  url: 'http://localhost:4200',
-};
+    url: 'spotify-luishidalgoa.vercel.app',
+  };
