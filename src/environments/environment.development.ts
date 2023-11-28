8e3c07a814589329b32294205e63108716e0b20c
Luis <luishidalgoa@outlook.es>
2023-11-28 14:43:02 +0100

    environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -12,5 +12,5 @@ export const environment = {
       key: 'Smh5aTVid29mc0FJcDZiWU5zeGVSeHhSdXNIZFUwMDFRZ2pweTZpUQ==',
     },
   },
-  url: 'http://localhost:4200',
+  url: 'https://spotify-luishidalgoa.vercel.app',
 };
