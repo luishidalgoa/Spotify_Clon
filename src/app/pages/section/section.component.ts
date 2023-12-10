ef48ee6ed0a2b9215d725b86c71605649c8bc89c
luishidalgoa <luishidalgoa@outlook.es>
2023-12-10 20:17:52 +0100

    LHA- components: Ahora la grilla de la pagina de secciones . Renderiza unicamente los elementos que caben dentro del maximo de columnas de la grilla

--- src/app/pages/section/section.component.ts
+++ src/app/pages/section/section.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BigCardComponent } from '../../components/cards/big-card/big-card.component';
+import { ReduceData } from '../../model/domain/api/spotify/reduce-data';
 
 @Component({
   selector: 'app-section',
@@ -11,11 +12,34 @@ import { BigCardComponent } from '../../components/cards/big-card/big-card.compo
 })
 export class SectionComponent {
   @Input({required: true}) 
-  value!: any;
+  items!: {title: string, data?: ReduceData[]};
+
+  wrapper!: ReduceData[];
+  
 
   constructor() { 
   }
   
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.onResize(null);
+    }, 100);
+  }
+
+  @ViewChild('container') 
+  container!: ElementRef;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    const cols = Math.floor(this.container.nativeElement.offsetWidth / 190);
+    this.wrapper = [];
+
+    this.items.data?.forEach((item: ReduceData, index: number) => {
+      if(index < cols){
+        this.wrapper.push(item);
+      }
+    });
   }
 }
